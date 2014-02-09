d9c6af0c96cc28d4935473c46bd4eb0f58ea582c
Jakousa <japekohatcher93@hotmail.com>
2014-02-09 16:46:01 +0200

    testeille omat luokat

---
+++ Tornipuolustus/src/test/java/tornipuolustus/toimijat/TorniTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tornipuolustus.toimijat;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Hatchy
+ */
+public class TorniTest {
+    
+    public TorniTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
